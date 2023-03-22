3e576a8050c66779b5a2ade819f72a2933f6a0fe
Andrew Hunn <ahunn59@gmail.com>
2023-03-22 15:30:17 -0400

    (chore) update server settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/",
 	plugins: [react()],
 	envDir: "./src",
 	assetsInclude: "**/*.md",
@@ -10,7 +11,7 @@ export default defineConfig({
 		watch: {
 			usePolling: true,
 		},
-		host: true, // needed for the Docker Container port mapping to work
+		host: "0.0.0.0", // needed for the Docker Container port mapping to work
 		strictPort: true,
 		port: 5173, // you can replace this port with any port
 	},
