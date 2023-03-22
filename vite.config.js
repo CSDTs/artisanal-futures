b2a503db2d932d1e5a1aa3e43e0ed4f86ed16223
Andrew Hunn <ahunn59@gmail.com>
2023-03-22 15:54:21 -0400

    (chore) update config for vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/",
 	plugins: [react()],
 	envDir: "./src",
 	assetsInclude: "**/*.md",
